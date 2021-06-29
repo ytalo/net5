c2f32d06b27dd943758c9c6af9f71bc0ea7d105d
Ytalo Jose Chamorro Colque <ytalo@Ytalos-MacBook-Air.local>
2021-06-29 12:38:18 -0500

    se agrega proyecto consola

---
+++ net5consola1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace net5Consola1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Console.ReadLine();
+
+            var ListaNombres = new[] { "Nombre1", "Nombre2", "Nombre3" };
+
+            foreach (var item in ListaNombres)
+            {
+                Console.WriteLine( $"Esta esta es una prueba para {item}" );
+            }
+        }
+    }
+}
